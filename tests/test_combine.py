f580d9cd618b22d3410d7e42bb173c5f86c051e4
Hansen <hansen@sidratresearch.com>
2023-07-20 13:43:35 -0400

    data existence reorganizing

--- tests/test_combine.py
+++ tests/test_combine.py
@@ -17,18 +17,26 @@ DATA_PATH = resources.files(__package__) / "test-data" / "TILES"
 """Path: Path to directory containing test data for this module.
 """
 
-IMAGE_PATH = DATA_PATH / "image.i.VAST_0334-37.SB50801.cont.taylor.0.restored.conv.fits"
+IMAGE_PATH = (
+    DATA_PATH
+    / "STOKESI_IMAGES"
+    / "image.i.VAST_0334-37.SB50801.cont.taylor.0.restored.conv.fits"
+)
 """Path: Path to random VAST epoch 38 image for testing purposes. 
 """
 
 REFERENCE_PATH = (
-    DATA_PATH / "image.i.VAST_0345-31.SB50802.cont.taylor.0.restored.conv.fits"
+    DATA_PATH
+    / "STOKESI_IMAGES"
+    / "image.i.VAST_0345-31.SB50802.cont.taylor.0.restored.conv.fits"
 )
 """Path: Path to random VAST epoch 38 image for header information in testing
 the function add_degenerate_axes().
 """
 
-WEIGHTS_PATH = DATA_PATH / "weights.i.VAST_0334-37.SB50801.cont.taylor.0.fits"
+WEIGHTS_PATH = (
+    DATA_PATH / "STOKESI_WEIGHTS" / "weights.i.VAST_0334-37.SB50801.cont.taylor.0.fits"
+)
 """Path: Path to VAST epoch 38 weights data corresponding to IMAGE_PATH image
 for testing the function mask_weightless_pixels().
 """
