2d24b9a8ed14abfff003bd5260a392d99176bc90
Dougal Dobie <ddobie@swin.edu.au>
2023-07-27 10:40:23 +1000

    Moved code out of run_crop CLI script to crop module.

--- vast_post_processing/cli/run_crop.py
+++ vast_post_processing/cli/run_crop.py
@@ -81,112 +81,14 @@ def main(
     
     logger.debug(locals())
     
-    image_path_glob_list: list[Generator[Path, None, None]] = []
-    
-    image_root = data_root / f"STOKES{stokes}_IMAGES"
-    logger.debug(image_root)
-    
-    if epoch is None or len(epoch) == 0:
-        image_path_glob_list.append(
-            image_root.glob(f"epoch_*/*.fits")
-        )
-    else:
-        for n in epoch:
-            image_path_glob_list.append(
-                image_root.glob(f"epoch_{n}/*.fits")
-            )
-    
-    for image_path in chain.from_iterable(image_path_glob_list):
-        logger.info(f"Working on {image_path}...")
-        epoch_dir = image_path.parent.name
-        _, _, field, sbid_str, *_ = image_path.name.split(".")
-        sbid = int(sbid_str[2:])
-        # get rms and background images
-        rms_path = (
-            data_root
-            / f"STOKES{stokes}_RMSMAPS"
-            / epoch_dir
-            / f"noiseMap.{image_path.name}"
-        )
-        bkg_path = (
-            data_root
-            / f"STOKES{stokes}_RMSMAPS"
-            / epoch_dir
-            / f"meanMap.{image_path.name}"
-        )
-        selavy_name = f"selavy-{image_path.name}".replace(".fits",
-                                                          ".components.xml"
-                                                          )           
-        components_path = (
-            data_root
-            / f"STOKES{stokes}_SELAVY"
-            / epoch_dir
-            / selavy_name
-        )
-        
-        #exists = rms_path.exists() and bkg_path.exists() and components_path.exists()
-        exists = components_path.exists()
-        if not exists:
-            logger.warning(f"Skipping {image_path}.")
-            continue
-        
-        
-        #for path in (rms_path, bkg_path, image_path):
-        for path in (image_path, ):
-            stokes_dir = f"{path.parent.parent.name}_CROPPED"
-            output_dir = out_root / stokes_dir / epoch_dir
-            
-            if not output_dir.exists():
-                output_dir.mkdir(parents=True)
-            
-            outfile = output_dir / path.name
-            hdu = fits.open(path)[0]
-            cropped_hdu = vpc.crop_hdu(hdu)
-            cropped_hdu.writeto(outfile, overwrite=overwrite)
-            logger.debug(f"Wrote {outfile}")
-        
-        
-        # Crop the catalogues
-        stokes_dir = f"{components_path.parent.parent.name}_CROPPED"
-        output_dir = out_root / stokes_dir / epoch_dir
-        
-        if not output_dir.exists():
-            output_dir.mkdir(parents=True)
-        
-        components_outfile = output_dir / components_path.name
-        vot = parse(str(components_path))
-        
-        # This uses the last cropped hdu from the above for loop
-        # which should be the image file, but doesn't actually matter
-        votable_cropped = vpc.crop_catalogue(vot, cropped_hdu)
-        if components_outfile.exists() and not overwrite:
-            logger.critical(f"{components_outfile} exists, not overwriting")
-        else:
-            vot.to_xml(str(components_outfile))
-            logger.debug(f"Wrote {components_outfile}")
-        
-        # Create the MOC
-        if not create_moc:
-            return
-
-        moc_dir = f"STOKES{stokes}_MOC_CROPPED"
-        output_dir = out_root / moc_dir / epoch_dir
-        
-        moc_filename = image_path.name.replace('.fits','.moc.fits')
-        moc_outfile = output_dir / moc_filename
-        
-        if not output_dir.exists():
-            output_dir.mkdir(parents=True)
-        moc = vpc.wcs_to_moc(cropped_hdu)
-        moc.write(moc_outfile, overwrite=overwrite)
-        logger.debug(f"Wrote {moc_outfile}")
-        
-        stmoc_filename = image_path.name.replace('.fits','.stmoc.fits')
-        stmoc_outfile = output_dir / stmoc_filename
-        
-        stmoc = vpc.moc_to_stmoc(moc, cropped_hdu)
-        stmoc.write(stmoc_outfile, overwrite=overwrite)
-        logger.debug("Wrote {stmoc_outfile}")
+    vpc.run_full_crop(data_root,
+                      crop_size*u.deg,
+                      epoch,
+                      stokes,
+                      out_root,
+                      create_moc
+                      overwrite
+                      )
 
 if __name__ == "__main__":
     typer.run(main)
