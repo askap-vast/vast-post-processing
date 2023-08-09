65036ccd2105dfe12ad0574c8b8599f72d322a0c
Hansen <hansen@sidratresearch.com>
2023-08-09 13:47:27 -0400

    separating logic - cleanup module

--- vast_post_processing/utils/fileutils.py
+++ vast_post_processing/utils/fileutils.py
@@ -2,5 +2,25 @@
 Utility Functions for Files
 """
 
-
 # Move cleanup functions/logic here
+
+from pathlib import Path
+from shutil import rmtree
+
+from loguru import logger
+
+
+def cleanup_directory(directory: Path):
+    DELETE_EXT = (".fits", ".ann", ".txt", ".xml")
+    DELETE_DIR = ("inputs", "tmp")
+
+    for path in directory.iterdir():
+        if path.is_file():
+            if path.suffix in DELETE_EXT:
+                path.unlink()
+                logger.info(f"Deleted file {path}.")
+        elif path.is_dir() and path.name in DELETE_DIR:
+            rmtree(path)
+            logger.info(f"Deleted directory {path}.")
+        else:
+            logger.debug(f"Leaving {path}.")
