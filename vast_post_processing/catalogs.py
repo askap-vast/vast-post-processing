2de93b994e94bbcbe4074cfed71dda736edf3a0e
Akash <aakash@uwm.edu>
2023-08-14 00:36:32 -0500

    Re-organized code so that this can be passed to cropping, added docstrings, cleaned the redundant parts of the code

--- vast_post_processing/catalogs.py
+++ vast_post_processing/catalogs.py
@@ -27,150 +27,37 @@ SELAVY_COLUMN_UNITS = {
     "rms_image": u.mJy / u.beam,
 }
 
-
-class UnknownCatalogInputFormat(Exception):
-    pass
-
-
-class Catalog:
-    CATALOG_TYPE_TILE = "TILE"
-    CATALOG_TYPE_COMBINED = "COMBINED"
-    CATALOG_TYPES = (
-        CATALOG_TYPE_TILE,
-        CATALOG_TYPE_COMBINED,
-    )
-
-    def __init__(
-        self,
-        path: Path,
-        psf: Optional[Tuple[float, float]] = None,
-        input_format: str = "selavy",
-        condon: bool = False,
-        positive_fluxes_only: bool = True,
-    ):
-        self.path: Path
-        self.table: QTable
-        self.field: Optional[str]
-        self.epoch: Optional[str]
-        self.sbid: Optional[str]
-        self.psf_major: Optional[u.Quantity]
-        self.psf_minor: Optional[u.Quantity]
-        self.type: str
-
-        # read catalog
-        if input_format == "selavy":
-            if path.suffix == ".txt":
-                logger.debug("Reading %s as a Selavy txt catalog.", path)
-                read_catalog = read_selavy
-            else:
-                logger.debug("Reading %s as a Selavy VOTable catalog.", path)
-                read_catalog = read_selavy_votable
-        elif input_format == "aegean":
-            logger.debug("Reading %s as an Aegean catalog.", path)
-            read_catalog = read_aegean_csv
-        else:
-            logger.error(
-                "The format of input files is not supported. Only selavy and aegean are supported"
-            )
-            raise SystemExit
-        self.path = path
-        self.table = read_catalog(path)
-
-        # filter sources with bad sizes and optionally negative/0 fluxes
-        if positive_fluxes_only:
-            logger.info(
-                "Filtering %d sources with fluxes <= 0.",
-                (self.table["flux_peak"] <= 0).sum(),
-            )
-            self.table = self.table[self.table["flux_peak"] > 0]
-        logger.info(
-            "Filtering %d sources with fitted sizes <= 0.",
-            ((self.table["maj_axis"] <= 0) | (self.table["min_axis"] <= 0)).sum(),
-        )
-        self.table = self.table[
-            (self.table["maj_axis"] > 0) & (self.table["min_axis"] > 0)
-        ]
-
-        # read epoch, field, sbid, psf's
-        epoch_name = path.parent.name
-        _, _, field, sbid, *_ = path.name.split(".")
-        self.epoch = epoch_name
-        self.field = field.replace("VAST_", "")
-        self.sbid = sbid
-
-        if psf is not None:
-            self.psf_major, self.psf_minor = psf * u.arcsec
-            logger.debug(
-                "Using user provided PSF for %s: %s, %s.",
-                self.path,
-                self.psf_major,
-                self.psf_minor,
-            )
-        else:
-            logger.warning(
-                "PSF is unknown for %s. Condon errors will be unavailable.", self.path
-            )
-            self.psf_major = None
-            self.psf_minor = None
-
-        if condon and self.psf_major is not None and self.psf_minor is not None:
-            _ = self.calculate_condon_flux_errors(correct_peak_for_noise=True)
-            logger.debug("Condon errors computed for %s.", self.path)
-
-    def calculate_condon_flux_errors(
-        self,
-        alpha_maj1=2.5,
-        alpha_min1=0.5,
-        alpha_maj2=0.5,
-        alpha_min2=2.5,
-        alpha_maj3=1.5,
-        alpha_min3=1.5,
-        clean_bias=0.0,
-        clean_bias_error=0.0,
-        frac_flux_cal_error=0.0,
-        correct_peak_for_noise=False,
-    ):
-        noise = self.table["rms_image"]
-        snr = self.table["flux_peak"] / noise
-
-        rho_sq3 = (
-            (
-                self.table["maj_axis"]
-                * self.table["min_axis"]
-                / (4.0 * self.psf_major * self.psf_minor)
-            )
-            * (1.0 + (self.psf_major / self.table["maj_axis"]) ** 2) ** alpha_maj3
-            * (1.0 + (self.psf_minor / self.table["min_axis"]) ** 2) ** alpha_min3
-            * snr**2
-        )
-
-        flux_peak_col = self.table["flux_peak"]
-        flux_peak_condon = self.table["flux_peak"] + (
-            -(noise**2) / self.table["flux_peak"] + clean_bias
-        )
-        if correct_peak_for_noise:
-            flux_peak_col = flux_peak_condon
-
-        errorpeaksq = (
-            (frac_flux_cal_error * flux_peak_col) ** 2
-            + clean_bias_error**2
-            + 2.0 * flux_peak_col**2 / rho_sq3
-        )
-        errorpeak = np.sqrt(errorpeaksq)
-
-        self.table["flux_peak_condon"] = flux_peak_condon
-        self.table["flux_peak_selavy"] = self.table["flux_peak"]
-        self.table["flux_peak_err_condon"] = errorpeak
-        self.table["flux_peak_err_selavy"] = self.table["flux_peak_err"]
-        self.table["flux_peak_err"] = self.table["flux_peak_err_condon"]
-        if correct_peak_for_noise:
-            self.table["flux_peak"] = self.table["flux_peak_condon"]
-        return flux_peak_condon, errorpeak
+AEGEAN_COLUMN_MAP = {
+    # aegean name: (selavy name, aegean unit)
+    "ra": ("ra_deg_cont", u.deg),
+    "dec": ("dec_deg_cont", u.deg),
+    "err_ra": ("ra_err", u.deg),
+    "err_dec": ("dec_err", u.deg),
+    "peak_flux": ("flux_peak", u.Jy / u.beam),
+    "err_peak_flux": ("flux_peak_err", u.Jy / u.beam),
+    "a": ("maj_axis", u.arcsec),
+    "b": ("min_axis", u.arcsec),
+    "pa": ("pos_ang", u.arcsec),
+    "err_a": ("maj_axis_err", u.arcsec),
+    "err_b": ("min_axis_err", u.deg),
+    "err_pa": ("pos_ang_err", u.deg),
+    "local_rms": ("rms_image", u.Jy / u.beam),
+}
 
 
 def _convert_selavy_columns_to_quantites(
     qt: QTable, units: Dict[str, u.Unit] = SELAVY_COLUMN_UNITS
 ) -> QTable:
+    """Takes in a selavy component table and adds units to respective quantities
+
+    Args:
+        qt (QTable): the component catalog
+        units (Dict[str, u.Unit], optional): The dictionary with parameters and
+        their units. Defaults to SELAVY_COLUMN_UNITS.
+
+    Returns:
+        QTable: Table with units to the parameters
+    """
     for col, unit in units.items():
         qt[col].unit = unit
     return qt
@@ -210,6 +97,14 @@ def read_selavy(catalog_path: Path) -> QTable:
 
 
 def read_selavy_votable(catalog_path: Path) -> QTable:
+    """Helper function to read the selavy catalog, if the input format is votable
+
+    Args:
+        catalog_path (Path): Input Path to the catalog file
+
+    Returns:
+        QTable: The component table
+    """
     t = Table.read(catalog_path, format="votable", use_names_over_ids=True)
     # remove units from str columns and fix unrecognized flux units
     for col in t.itercols():
@@ -218,17 +113,6 @@ def read_selavy_votable(catalog_path: Path) -> QTable:
         elif col.unit == u.UnrecognizedUnit("mJy/beam"):
             col.unit = u.Unit("mJy/beam")
     qt = QTable(t)
-    qt["coord"] = SkyCoord(ra=qt["ra_deg_cont"], dec=qt["dec_deg_cont"])
-    _, qt["nn_separation"], _ = qt["coord"].match_to_catalog_sky(
-        qt["coord"], nthneighbor=2
-    )
-    return qt
-
-
-def read_hdf(catalog_path: Path) -> pd.DataFrame:
-    df = pd.read_hdf(catalog_path, key="data")
-    df["field"] = df.field.str.split(".", n=1, expand=True)[0]
-    qt = _convert_selavy_columns_to_quantites(QTable.from_pandas(df))
     qt["coord"] = SkyCoord(ra=qt["ra_deg_cont"], dec=qt["dec_deg_cont"])
     _, qt["nn_separation"], _ = qt["coord"].match_to_catalog_sky(
         qt["coord"], nthneighbor=2
@@ -260,22 +144,6 @@ def read_aegean_csv(catalog_path: Path) -> QTable:
         - `nn_separation`: separation to the nearest-neighbour source as a Quantity with
             angular units.
     """
-    AEGEAN_COLUMN_MAP = {
-        # aegean name: (selavy name, aegean unit)
-        "ra": ("ra_deg_cont", u.deg),
-        "dec": ("dec_deg_cont", u.deg),
-        "err_ra": ("ra_err", u.deg),
-        "err_dec": ("dec_err", u.deg),
-        "peak_flux": ("flux_peak", u.Jy / u.beam),
-        "err_peak_flux": ("flux_peak_err", u.Jy / u.beam),
-        "a": ("maj_axis", u.arcsec),
-        "b": ("min_axis", u.arcsec),
-        "pa": ("pos_ang", u.arcsec),
-        "err_a": ("maj_axis_err", u.arcsec),
-        "err_b": ("min_axis_err", u.deg),
-        "err_pa": ("pos_ang_err", u.deg),
-        "local_rms": ("rms_image", u.Jy / u.beam),
-    }
     qt = QTable.read(catalog_path)
     # rename columns to match selavy convention and assign units
     for col, (new_col, unit) in AEGEAN_COLUMN_MAP.items():
@@ -296,3 +164,178 @@ def read_aegean_csv(catalog_path: Path) -> QTable:
         qt["coord"], nthneighbor=2
     )
     return qt
+
+
+class Catalog:
+    """Class to make a catalog object from the selavy/Aegean files. This
+       is then used for catalog matching between the referecne catalog and
+       the current catalog to select for sources and get flux and astrometric
+       corrections.
+
+    Raises:
+        SystemExit: if the input catalog files are other than Selavy/Aegean
+                    products
+    """
+
+    def __init__(
+        self,
+        path: Path,
+        psf: Optional[Tuple[float, float]] = None,
+        input_format: str = "selavy",
+        condon: bool = False,
+        apply_flux_limit: bool = True,
+        flux_limit: float = 0,
+    ):
+        self.path: Path
+        self.table: QTable
+        self.input_format: Optional[str]
+        self.flux_flag: Optional[bool]
+        self.flux_lim: Optional[float]
+        self.field: Optional[str]
+        self.epoch: Optional[str]
+        self.sbid: Optional[str]
+        self.psf_major: Optional[u.Quantity]
+        self.psf_minor: Optional[u.Quantity]
+        self.type: str
+
+        self.path = path
+        self.input_format = input_format
+        self.flux_flag = apply_flux_limit
+        self.flux_lim = flux_limit
+
+        # Read the catalog
+        self._read_catalog()
+
+        # Filter sources
+        self._filter_sources()
+
+        # Get epoch, field, sbid from the file name
+        epoch_name = path.parent.name
+        _, _, field, sbid, *_ = path.name.split(".")
+        self.epoch = epoch_name
+        self.field = field.replace("VAST_", "")
+        self.sbid = sbid
+
+        # Parse the psf info
+        if psf is not None:
+            self.psf_major, self.psf_minor = psf * u.arcsec
+            logger.debug(
+                "Using user provided PSF for %s: %s, %s.",
+                self.path,
+                self.psf_major,
+                self.psf_minor,
+            )
+        else:
+            logger.warning(
+                "PSF is unknown for %s. Condon errors will be unavailable.", self.path
+            )
+            self.psf_major = None
+            self.psf_minor = None
+
+        # Calculate the covariant error using Condon 1997
+        if condon and self.psf_major is not None and self.psf_minor is not None:
+            self.calculate_condon_flux_errors(correct_peak_for_noise=True)
+            logger.debug("Condon errors computed for %s.", self.path)
+
+    def _read_catalog(self):
+        """Helper function to read and parse the input files
+
+        Raises:
+            SystemExit: if the input catalog files are other than Selavy/Aegean
+                        products
+        """
+        path = self.path
+        if self.input_format == "selavy":
+            if path.suffix == ".txt":
+                logger.debug("Reading %s as a Selavy txt catalog.", path)
+                read_catalog = read_selavy
+            else:
+                logger.debug("Reading %s as a Selavy VOTable catalog.", path)
+                read_catalog = read_selavy_votable
+        elif self.input_format == "aegean":
+            logger.debug("Reading %s as an Aegean catalog.", path)
+            read_catalog = read_aegean_csv
+        else:
+            logger.error(
+                "The format of input files is not supported. Only selavy and aegean are supported"
+            )
+            raise SystemExit
+
+        self.table = read_catalog(path)
+
+    def _filter_sources(self):
+        """Helper function to filter sources that are used for cross-match;
+        filter sources with bad sizes and optionally given flux limits"""
+        if self.flux_flag:
+            lim = self.flux_lim
+            logger.info(
+                f"Filtering %d sources with fluxes <= {lim}",
+                (self.table["flux_peak"] <= lim).sum(),
+            )
+            self.table = self.table[self.table["flux_peak"] > lim]
+        logger.info(
+            "Filtering %d sources with fitted sizes <= 0.",
+            ((self.table["maj_axis"] <= 0) | (self.table["min_axis"] <= 0)).sum(),
+        )
+        self.table = self.table[
+            (self.table["maj_axis"] > 0) & (self.table["min_axis"] > 0)
+        ]
+
+    def calculate_condon_flux_errors(
+        self,
+        alpha_maj: float = 1.5,
+        alpha_min: float = 1.5,
+        clean_bias: float = 0.0,
+        clean_bias_error: float = 0.0,
+        frac_flux_cal_error: float = 0.0,
+        correct_peak_for_noise: bool = False,
+    ):
+        """Calculates the covariant error using Condon 1997. See equation 41
+        of Condon 1997 for reference
+
+        Args:
+            alpha_maj (float, optional): power for major axis correction. Defaults to 1.5
+            alpha_min (float, optional): power for major axis correction. Defaults to 1.5.
+            clean_bias (float, optional): additive flux bias. Defaults to 0.0.
+            clean_bias_error (float, optional): error in additive flux bias. Defaults to 0.0.
+            frac_flux_cal_error (float, optional): multiplicative flux error. Defaults to 0.0.
+            correct_peak_for_noise (bool, optional): flag to re-write the peak flux from
+            selavy. Defaults to False.
+        """
+        noise = self.table["rms_image"]
+        snr = self.table["flux_peak"] / noise
+
+        # See equation 41 of Condon 1997 to calculate the signal to noise
+        rho_sq3 = (
+            (
+                self.table["maj_axis"]
+                * self.table["min_axis"]
+                / (4.0 * self.psf_major * self.psf_minor)
+            )
+            * (1.0 + (self.psf_major / self.table["maj_axis"]) ** 2) ** alpha_maj
+            * (1.0 + (self.psf_minor / self.table["min_axis"]) ** 2) ** alpha_min
+            * snr**2
+        )
+
+        # Correct the peak flux now.
+        flux_peak_col = self.table["flux_peak"]
+        flux_peak_condon = self.table["flux_peak"] + (
+            -(noise**2) / self.table["flux_peak"] + clean_bias
+        )
+        if correct_peak_for_noise:
+            flux_peak_col = flux_peak_condon
+
+        errorpeaksq = (
+            (frac_flux_cal_error * flux_peak_col) ** 2
+            + clean_bias_error**2
+            + 2.0 * flux_peak_col**2 / rho_sq3
+        )
+        errorpeak = np.sqrt(errorpeaksq)
+
+        self.table["flux_peak_condon"] = flux_peak_condon
+        self.table["flux_peak_selavy"] = self.table["flux_peak"]
+        self.table["flux_peak_err_condon"] = errorpeak
+        self.table["flux_peak_err_selavy"] = self.table["flux_peak_err"]
+        self.table["flux_peak_err"] = self.table["flux_peak_err_condon"]
+        if correct_peak_for_noise:
+            self.table["flux_peak"] = self.table["flux_peak_condon"]
