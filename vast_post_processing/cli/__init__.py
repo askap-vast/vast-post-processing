7175c396b3ecbbc8eeadba95d4d49c6deefaabfc
Hansen <hansen@sidratresearch.com>
2023-08-16 23:54:06 -0400

    removed deleted correct_vast from imports

--- vast_post_processing/cli/__init__.py
+++ vast_post_processing/cli/__init__.py
@@ -14,7 +14,6 @@ Program logic located in base modules.
 from . import _util
 from . import cleanup
 from . import convolve_neighbours
-from . import correct_vast
 from . import link_neighbours
 from . import run_crop
 from . import selavy_combined
