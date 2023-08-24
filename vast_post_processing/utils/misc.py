603bc6320ef110ece137a9e77fe3c70816c64951
Hansen <hansen@sidratresearch.com>
2023-08-24 16:59:31 -0400

    [WIP] reorganized functions, added docstrings, comments

--- vast_post_processing/utils/misc.py
+++ vast_post_processing/utils/misc.py
@@ -1,1 +1,15 @@
-# Miscellaneous Utilities
+"""
+
+ Miscellaneous Utilities for VAST Post Processing
+
+"""
+from pathlib import Path
+from typing import Tuple
+
+def get_epoch_directory(image_path: Path) -> str:
+    return image_path.parent.name
+
+def get_field_and_sbid(image_path: Path) -> Tuple[str,int]:
+    _, _, field, sbid_str, *_ = image_path.name.split(".")
+    sbid = int(sbid_str[2:])
+    return field, sbid
