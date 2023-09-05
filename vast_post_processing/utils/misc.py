fa7d33fcd0c003b8ba3eedf92a1a53403c39758a
Hansen <hansen@sidratresearch.com>
2023-09-05 16:58:42 -0400

    [WIP] common constants and fixtures for tests

--- vast_post_processing/utils/misc.py
+++ vast_post_processing/utils/misc.py
@@ -6,10 +6,12 @@
 from pathlib import Path
 from typing import Tuple
 
+
 def get_epoch_directory(image_path: Path) -> str:
     return image_path.parent.name
 
-def get_field_and_sbid(image_path: Path) -> Tuple[str,int]:
+
+def get_field_and_sbid(image_path: Path) -> Tuple[str, int]:
     _, _, field, sbid_str, *_ = image_path.name.split(".")
     sbid = int(sbid_str[2:])
     return field, sbid
