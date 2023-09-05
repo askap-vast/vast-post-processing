d6ecec3723a32251640a5818d2bd12cd01e67688
Hansen <hansen@sidratresearch.com>
2023-09-05 16:25:55 -0400

    [WIP] add tests for core and utils

---
+++ tests/vast_post_processing/test_core.py
@@ -0,0 +1,54 @@
+"""Test the module `vast_post_processing.core`. 
+"""
+
+import pytest
+
+from vast_post_processing import core
+
+
+def test_verify_correct_type():
+    pass
+
+
+@pytest.mark.xfail(
+    reason="",
+    strict=True,
+)
+def test_fail_verify_correct_type():
+    pass
+
+
+def test_setup_configuration_variable():
+    pass
+
+
+def test_setup_configuration():
+    pass
+
+
+def test_setup_logger():
+    pass
+
+
+def test_get_image_paths():
+    pass
+
+
+def test_get_corresponding_paths():
+    pass
+
+
+def test_crop_and_correct_image():
+    pass
+
+
+def test_crop_catalogs():
+    pass
+
+
+def test_create_mocs():
+    pass
+
+
+def test_run():
+    pass
