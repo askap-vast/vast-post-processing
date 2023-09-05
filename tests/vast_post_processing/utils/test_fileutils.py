0987f8b15f97683b5a3ce3d17f8eaf55a19a9407
Hansen <hansen@sidratresearch.com>
2023-09-05 16:25:55 -0400

    [WIP] add tests for core and utils

---
+++ tests/vast_post_processing/utils/test_fileutils.py
@@ -0,0 +1,14 @@
+"""Test the module `vast_post_processing.utils.fileutils`.
+"""
+
+import pytest
+
+from vast_post_processing.utils import fileutils
+
+
+def test_cleanup_directory():
+    pass
+
+
+def test_cleanup():
+    pass
---
+++ tests/vast_post_processing/utils/test_misc.py
@@ -0,0 +1,14 @@
+"""Test the module `vast_post_processing.utils.misc`.
+"""
+
+import pytest
+
+from vast_post_processing.utils import misc
+
+
+def test_get_epoch_directory():
+    pass
+
+
+def test_get_field_and_sbid():
+    pass
