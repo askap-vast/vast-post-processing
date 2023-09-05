1212d3696d96c6bf0024e7e346ea0c5f10f5fdd6
Hansen <hansen@sidratresearch.com>
2023-09-05 16:58:42 -0400

    [WIP] common constants and fixtures for tests

--- tests/vast_post_processing/utils/test_misc.py
+++ tests/vast_post_processing/utils/test_misc.py
@@ -1,13 +1,15 @@
 """Test the module `vast_post_processing.utils.misc`.
 """
 
-import pytest
-
 from vast_post_processing.utils import misc
 
 
 def test_get_epoch_directory():
-    pass
+    epoch = misc.get_epoch_directory(image_path)
+
+    correct_type = isinstance(epoch, str)
+    correct_format = epoch[:6] == "epoch_"
+    correct_epoch = epoch[6:] == "38"
 
 
 def test_get_field_and_sbid():
