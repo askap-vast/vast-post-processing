2cf36e81b62d030fab6ce459b13e99b9d21df707
Hansen <hansen@sidratresearch.com>
2023-09-14 00:51:42 -0400

    update docstring, conditionals

--- vast_post_processing/utils/fitsutils.py
+++ vast_post_processing/utils/fitsutils.py
@@ -5,31 +5,34 @@ Utility functions for handling fits files
 from astropy.time import Time, TimeDelta
 from astropy.io import fits
 
-def update_header_datetimes(header: fits.header.Header):
-    """Take a fits header containing DATE-OBS and DURATION keys and add
-    the other relevant datetime keys.
-    
+
+def update_header_datetimes(header: fits.Header):
+    """Add relevant observation datetime headers to a FITS header containing
+    DATE-OBS and DURATION keys.
+
     Parameters
     ----------
     header: fits.header.Header
-        The header to update
-    """
+        The FITS header to update.
 
-    if "DATE-OBS" not in header.keys()
+    Raises
+    ------
+    ValueError
+        If expected datetime headers DATE-OBS and/or DURATION are missing.
+    """
+    if "DATE-OBS" not in header.keys():
         raise ValueError("DATE-OBS must be in header keys")
-    if "DURATION" not in header.keys()
+    if "DURATION" not in header.keys():
         raise ValueError("DURATION must be in header keys")
 
     obs_start = Time(header["DATE-OBS"])
-    duration = TimeDelta(header["DURATION"], format='sec')
+    duration = TimeDelta(header["DURATION"], format="sec")
     obs_end = obs_start + duration
-    
+
     header["MJD-OBS"] = obs_start.mjd
     header["DATE-BEG"] = obs_start.fits
     header["DATE-END"] = obs_end.fits
     header["MJD-BEG"] = obs_start.mjd
     header["MJD-END"] = obs_end.mjd
     header["TELAPSE"] = duration.sec
-    header["TIMEUNIT"] =  "s"
-    
-    return
+    header["TIMEUNIT"] = "s"
