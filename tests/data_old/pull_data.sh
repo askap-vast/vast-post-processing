449bbb9a09e72220df3c39cd13b1960d5416e069
Hansen <hansen@sidratresearch.com>
2023-09-21 16:44:17 -0400

    [WIP] move old test data tests to temp folder

--- tests/data_old/pull_data.sh
+++ tests/data_old/pull_data.sh
@@ -15,3 +15,5 @@ cd ../STOKESI_WEIGHTS
 echo Downloading corresponding weight data from VAST data server.
 rsync ubuntu@data.vast-survey.org:/mnt/data/VAST/release/EPOCH38/TILES/STOKESI_WEIGHTS/weights.i.VAST_0334-37.SB50801.cont.taylor.0.fits . -h --progress
 echo Done. Check your test-data directory. 
+
+# TODO update script for current data
