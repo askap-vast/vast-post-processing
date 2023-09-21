bca8395f25dc9b43bad6e8b6117619b5dd411ae8
Hansen <hansen@sidratresearch.com>
2023-09-21 16:46:02 -0400

    [WIP] #31 - script to pull data from server

---
+++ tests/data/pull_data.py
@@ -0,0 +1,33 @@
+"""Scripts and tools to download test data from data.vast-survey.org.
+
+Note
+----
+These actions require the correct permissions, notably access to the data
+server. See the [VAST
+Wiki](https://github.com/askap-vast/vast-project/wiki/Nimbus:-SSH-&-Downloading-Data)
+for more information. 
+"""
+
+
+# Imports
+
+
+import yaml
+import pytest
+from pathlib import Path
+
+
+# Constants
+
+
+DATA_ROOT = "/mnt/data/VAST/vast-data/TILES"
+"""Path to root directory containing subdirectories and observation data on
+the vast data server.
+"""
+
+
+# Functions
+
+
+def main():
+    pass
