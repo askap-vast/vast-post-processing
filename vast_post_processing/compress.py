95be224e63ed563ef405e30cbc1c62efa6a99cb9
Hansen <hansen@sidratresearch.com>
2023-09-28 14:35:47 -0400

    #32 - compress

--- vast_post_processing/compress.py
+++ vast_post_processing/compress.py
@@ -1,37 +1,48 @@
+"""Compress image HDU.
+"""
+
+
+# Imports
+
+
 from astropy.io import fits
 from astropy.wcs import WCS
 from datetime import datetime
 
-def compress_hdu(hdu: fits.ImageHDU, quantize_level: float=1024.0, **kwargs):
+
+# Functions
+
+
+def compress_hdu(
+    hdu: fits.ImageHDU, quantize_level: float = 1024.0, **kwargs
+) -> fits.CompImageHDU:
     """Convert a given astropy HDU into a compressed HDU.
-    
+
     Parameters
     ----------
-        hdu : fits.ImageHDU
-            The HDU to compress
-        quantize_level : float
-            The quantization level to use for compression
-        **kwargs
-            Keyword arguments to be passed onto `astropy.io.fits.CompImageHDU`
-    
-    Returns
-    ----------
-    CompImageHDU
-        The resulting CompImageHDU
-    """
+    hdu : fits.ImageHDU
+        HDU to be compressed.
+    quantize_level : float, optional
+        Quantization level of compression, by default 1024.0.
 
+    Returns
+    -------
+    fits.CompImageHDU
+        Resulting CompImageHDU.
+    """
+    # Update header WCS
     header = hdu.header
     wcs = WCS(header, naxis=2)
     header.update(wcs.to_header())
+
+    # Update header history
     header_str = f"Compressed and reduced to NAXIS=2 on {datetime.now()}"
     header.add_history(header_str)
 
+    # Remove empty/degenerated axes from data
     data = hdu.data.squeeze()
 
-    comp_hdu = fits.CompImageHDU(data=data,
-                                 header=header,
-                                 quantize_level=quantize_level,
-                                 **kwargs
-                                 )
-    
-    return comp_hdu
+    # Return compressed data HDU
+    return fits.CompImageHDU(
+        data=data, header=header, quantize_level=quantize_level, **kwargs
+    )
